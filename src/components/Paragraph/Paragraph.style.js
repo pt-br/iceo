2f0987c5465b55c6ab6acd8646ff0edd5d4e922a
pt-br <ltavares@yieldstreet.com>
2023-06-14 15:19:23 -0300

    Default style for paragraph

--- src/components/Paragraph/Paragraph.style.js
+++ src/components/Paragraph/Paragraph.style.js
@@ -1,9 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import { colors } from 'utils';
+
+const DefaultStyle = css`
+  color: ${colors.gray_6};
+`;
 
 export const Regular = styled.div`
+  ${DefaultStyle};
   font-size: 16px;
 `;
 
 export const Small = styled.div`
+  ${DefaultStyle};
   font-size: 14px;
 `;
