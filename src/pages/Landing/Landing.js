f386b7a9ef3480823c3b81193941b0f8b1e71b90
italomf-yieldstreet <ifranco@yieldstreet.com>
2023-06-14 15:15:58 -0300

    graph colors and mock

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,12 +1,14 @@
 import { Page } from 'components/Page';
 import { Heading } from 'components/Heading';
 import { Paragraph } from 'components/Paragraph';
+import { Results } from 'pages/Results';
 
 export const Landing = () => {
   return (
     <Page>
       <Heading>heading</Heading>
       <Paragraph>paragraphhhhh</Paragraph>
+      <Results />
     </Page>
   );
 };
