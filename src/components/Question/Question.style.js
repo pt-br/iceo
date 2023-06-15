c16d0a73ef648622152f2b0ffcfa800813e365a6
italomf-yieldstreet <ifranco@yieldstreet.com>
2023-06-15 15:13:51 -0300

    mobile for slider

--- src/components/Question/Question.style.js
+++ src/components/Question/Question.style.js
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
-import { colors } from 'utils';
+import { colors, media } from 'utils';
 
 export const QuestionWrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   padding: 32px 52px;
   gap: 30px;
   justify-content: space-between;
   background: ${props =>
     props.background ? colors.light_grey : colors.white};
+  ${media.desktop`
+    flex-direction: row;
+  `}
 `;
 
 export const QuestionDetails = styled.div`
