f530d272c4b5bc0cb0fc843b6ce370c2b4d7488e
italomf-yieldstreet <ifranco@yieldstreet.com>
2023-06-15 16:31:56 -0300

    labels

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,7 @@
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import HighchartsMore from 'highcharts/highcharts-more';
-import { ChartWrapper } from './Chart.style';
+import { ChartWrapper, LeadershipTitle, RoleTitle } from './Chart.style';
 
 HighchartsMore(Highcharts);
 
@@ -9,6 +9,8 @@ const Chart = ({ options }) => {
   return (
     <ChartWrapper>
       <HighchartsReact highcharts={Highcharts} options={options} />
+      <LeadershipTitle>Leadership & Management</LeadershipTitle>
+      <RoleTitle>Role Specifics</RoleTitle>
     </ChartWrapper>
   );
 };
--- src/components/Chart/Chart.style.js
+++ src/components/Chart/Chart.style.js
@@ -1,7 +1,22 @@
 import styled from 'styled-components';
 
 export const ChartWrapper = styled.div`
+  position: relative;
   div {
     height: 600px;
   }
+`;
+
+export const LeadershipTitle = styled.div`
+  position: absolute;
+  font-weight: 600;
+  top: 41%;
+  right: -83px;
+`;
+
+export const RoleTitle = styled.div`
+  position: absolute;
+  font-weight: 600;
+  top: 40%;
+  left: -30px;
 `;
